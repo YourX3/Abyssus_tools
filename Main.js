febe9e66336e9c533862e2912454486d00bec8f2
YourX3 <39052073+YourX3@users.noreply.github.com>
2018-05-08 12:19:49 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,10 +3,9 @@ update();
 
 function update(){
 	var docSearchPath = document.location.search;
-	
+
 	if(docSearchPath.split('&')[0] === "?page=attaque" && docSearchPath.split('&')[2] === "lieu=1")
 	{
-		alert(docSearchPath);
 		page_atk();
 	}
 };
@@ -18,14 +17,15 @@ function update(){
 
 function page_atk()
 {
- 	var playerName = document.getElementsByTagName('h1')[0].split(' ')[3];
-	alert(document.getElementsByTagName('h1')[0]);
-	
-	var inputTdcTarget = "<input type=\"text" id=\"targetTdc\" name=\"targetTdc\" class=\"text\" value=\"tdc de la cible\" data-nb=\"0\" style=\"font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; color: rgb(0, 0, 102); text-align: center; outline: none; padding: 5px; width: 120px; cursor: text;\">"
-	document.getElementsByTagName('center')[0].appendChild(inputTdcTarget);
-};
+ 	var playerName = document.getElementsByTagName('h1')[0].textContent.split(' ')[3];
+	alert(playerName);
 
 
+	var inputTdcTarget = document.createElement('none');
+    inputTdcTarget.innerHTML =  '<input type="text" id="targetTdc" name="targetTdc" class="text" value="tdc de la cible" data-nb="0" style="font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; color: rgb(0, 0, 102); text-align: center; outline: none; padding: 5px; width: 120px; cursor: text;">';
+
+    document.getElementsByTagName('center')[0].insertBefore(inputTdcTarget, document.getElementsByTagName('h1')[0]);
+}
 
 
 /*********************************************************
