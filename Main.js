e879701c77917979cae6313ea021d92c3be1f91e
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-05-15 21:01:39 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -14,7 +14,7 @@ init();
 // fonction appelée lorsque la page est chargée(sur https://s1.abyssus.games/*)
 function init(){
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.3 __ Last Updtate 15/05/2018  20h16 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.3 __ Last Updtate 15/05/2018  20h56 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -345,31 +345,17 @@ function page_playerProfile(){
 	
 	if(localStorage.getItem("userId") !== null){
 		if(localStorage.getItem("userId") !== document.location.search.split('&')[1]){
-			var textPosPlayer = document.getElementsByTagName('tbody')[1].childNodes[3].innerText;
-			var posXx = textPosPlayer.split(' ')[3];
-			var posX = "";
-			for(var i =0; i < posXx.length; ++i){
-				if(posXx[i] == '+' || posXx[i] == '-' || !isNaN(Number(posXx[i])))
-				{
-					posX += posXx[i];
-				}
-			}
-			posX = Number(posX);
-			var posY = Number(textPosPlayer.split(' ')[6]);
-			
-			var userPosX = Number(localStorage.getItem("userPos").split(' ')[0]);
-			var userPosY = Number(localStorage.getItem("userPos").split(' ')[1]);
-			var dist = Math.round(Math.sqrt((userPosX-posX)*(userPosX-posX) + (userPosY-posY)*(userPosY-posY)) + 0.5);
-			
-			document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].innerText += " distance: " + String(dist);
-			
+					
 			var playerTM = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].innerText;
 			Number.prototype.nombreFormate=function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 			var maxTM = (Number(playerTM.replace(/\s/g, ''))+1).nombreFormate(0);
 
 			$.post('ajax/ennemies.php', {mintdc:playerTM, maxtdc:maxTM, page:1, tri:'distance', sens:'asc', guerre:0, paix:0, ally:0}, function(data){
-				var time = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].innerText)[4];
-				document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].innerText += " _ Temps de trajet: " + time;
+				var listOfResults = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].innerText);
+				var distance = listOfResults[3];
+				var time = listOfResults[4];
+				
+				document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].innerText += " Distance: " + distance + "&nbsp;" +"&nbsp;" +"&nbsp;" +"&nbsp;"+ "&nbsp;" + " _ Temps de trajet: " + time;
 			});
 		}
 	}
