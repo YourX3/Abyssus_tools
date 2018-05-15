ab08a9c4944ba18351996694a586f2f35e9cbdad
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-05-15 21:06:24 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -342,36 +342,18 @@ function onClickButtonReplaceArmy(){
 	
 function page_playerProfile(){
 	document.getElementsByTagName('tbody')[1].childNodes[9].childNodes[1].childNodes[8].onclick = function(){onclick_tmAttack()};
-	
-	if(localStorage.getItem("userId") !== null){
-		if(localStorage.getItem("userId") !== document.location.search.split('&')[1]){
-					
-			var playerTM = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].innerText;
-			Number.prototype.nombreFormate=function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
-			var maxTM = (Number(playerTM.replace(/\s/g, ''))+1).nombreFormate(0);
-
-			$.post('ajax/ennemies.php', {mintdc:playerTM, maxtdc:maxTM, page:1, tri:'distance', sens:'asc', guerre:0, paix:0, ally:0}, function(data){
-				var listOfResults = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].innerText);
-				var distance = listOfResults[3];
-				var time = listOfResults[4];
 				
-				document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].innerText += " Distance: " + distance + "&nbsp;" +"&nbsp;" +"&nbsp;" +"&nbsp;"+ "&nbsp;" + " _ Temps de trajet: " + time;
-			});
-		}
-	}
-	
-	var button_ItsMe = document.createElement('none');
-	button_ItsMe.innerHTML = '<button name="buttonItsMe" onclick="onclick_buttonItsMe()">C est moi !</button>';
-	document.getElementsByTagName('center')[0].insertBefore(button_ItsMe, document.getElementsByTagName('table')[1]);
-	
-	var textNameUser = document.createElement('none');
-	var userName = " ( " + localStorage.getItem("userName") + " )";
-	if(userName === null){
-		userName = " (pas encore defini)"
-	}
-	textNameUser.innerHTML = '<font color="white" name="userName"> (..) </font>';
-	textNameUser.innerText = userName;
-	document.getElementsByTagName('center')[0].insertBefore(textNameUser, document.getElementsByTagName('table')[1]);
+	var playerTM = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].innerText;
+	Number.prototype.nombreFormate=function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
+	var maxTM = (Number(playerTM.replace(/\s/g, ''))+1).nombreFormate(0);
+
+	$.post('ajax/ennemies.php', {mintdc:playerTM, maxtdc:maxTM, page:1, tri:'distance', sens:'asc', guerre:0, paix:0, ally:0}, function(data){
+		var listOfResults = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].innerText);
+		var distance = listOfResults[3];
+		var time = listOfResults[4];
+
+		document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].innerText += " Distance: " + distance + "&nbsp;" +"&nbsp;" +"&nbsp;" +"&nbsp;"+ "&nbsp;" + " _ Temps de trajet: " + time;
+	});
 }
 
 function setPlayerTravelTime(data, constraint = "none"){
@@ -459,29 +441,6 @@ function setPlayerTravelTime(data, constraint = "none"){
 	}
 	return finalValue;
 }
-
-function onclick_buttonItsMe(){
-	localStorage.setItem('userId', document.location.search.split('&')[1]);
-	
-	var textPosPlayer = document.getElementsByTagName('tbody')[1].childNodes[3].innerText;
-	var posXx = textPosPlayer.split(' ')[3];
-	var posX = "";
-	for(var i =0; i < posXx.length; ++i){
-		if(posXx[i] == '+' || posXx[i] == '-' || !isNaN(Number(posXx[i])))
-		{
-			posX += posXx[i];
-		}
-	}
-	var posY = textPosPlayer.split(' ')[6];
-	
-	var position = posX + " " + posY;
-	localStorage.setItem('userPos', position);
-	
-	localStorage.setItem("userName", document.getElementsByTagName('h1')[0].innerText);
-	
-	document.getElementsByName('userName')[0].innertText = " ( " + document.getElementsByTagName('h1')[0].innerText + " )";
-}
-	
 
 function onclick_tmAttack(){
 	var tmTarget = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].innerText;
