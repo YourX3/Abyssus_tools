45033f7d728f1a5aa4162885cb524bb718b8ceef
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-03 19:18:54 +0200

    Update Abyssus_Tools_enDev.js

--- Abyssus_Tools_enDev.js
+++ Abyssus_Tools_enDev.js
@@ -14,7 +14,7 @@ init();
 // fonction appelée lorsque la page est chargée(sur https://s1.abyssus.games/*)
 function init(){
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.3 __ Last Updtate 03/06/2018 19h12 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.3 __ Last Updtate 03/06/2018 19h18 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -707,28 +707,30 @@ function page_prodUnit(){
 }
 
 function updateTables(){
-	var tables = getElementsByTagNameInList(document.getElementById("bloc").childNodes, "TABLE");
-	if(tables.length > 0){
-		for(var i=0; i < tables.length; ++i){
-			var trs = getElementsByTagNameInList(tables[i].childNodes[1].childNodes, "TR");
+	setTimeout(function(){
+		var tables = getElementsByTagNameInList(document.getElementById("bloc").childNodes, "TABLE");
+		if(tables.length > 0){
+			for(var i=0; i < tables.length; ++i){
+				var trs = getElementsByTagNameInList(tables[i].childNodes[1].childNodes, "TR");
 
-			if(localStorage.getItem("ecaille") !== null){
-				var hpsValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[1].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
-				hpsValue = " " + String(Number(removeSpaces(hpsValue)) + Number(removeSpaces(hpsValue)) * (Number(localStorage.getItem("ecaille"))/10))
-				getElementsByTagNameInList(getElementsByTagNameInList(trs[1].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = hpsValue;
-			}
-			if(localStorage.getItem("morsure") !== null){
-				var atkValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[2].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
-				atkValue = " " + String(Number(removeSpaces(atkValue)) + Number(removeSpaces(atkValue)) * (Number(localStorage.getItem("morsure"))/10))
-				getElementsByTagNameInList(getElementsByTagNameInList(trs[2].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = atkValue;
-			}
-			if(localStorage.getItem("morsure") !== null){
-				var defValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[3].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
-				defValue = " " + String(Number(removeSpaces(defValue)) + Number(removeSpaces(defValue)) * (Number(localStorage.getItem("morsure"))/10))
-				getElementsByTagNameInList(getElementsByTagNameInList(trs[3].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = defValue;
+				if(localStorage.getItem("ecaille") !== null){
+					var hpsValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[1].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
+					hpsValue = " " + String(Number(removeSpaces(hpsValue)) + Number(removeSpaces(hpsValue)) * (Number(localStorage.getItem("ecaille"))/10))
+					getElementsByTagNameInList(getElementsByTagNameInList(trs[1].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = hpsValue;
+				}
+				if(localStorage.getItem("morsure") !== null){
+					var atkValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[2].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
+					atkValue = " " + String(Number(removeSpaces(atkValue)) + Number(removeSpaces(atkValue)) * (Number(localStorage.getItem("morsure"))/10))
+					getElementsByTagNameInList(getElementsByTagNameInList(trs[2].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = atkValue;
+				}
+				if(localStorage.getItem("morsure") !== null){
+					var defValue = getElementsByTagNameInList(getElementsByTagNameInList(trs[3].childNodes, "TD")[0].childNodes, "SPAN")[0].innerText;
+					defValue = " " + String(Number(removeSpaces(defValue)) + Number(removeSpaces(defValue)) * (Number(localStorage.getItem("morsure"))/10))
+					getElementsByTagNameInList(getElementsByTagNameInList(trs[3].childNodes, "TD")[0].childNodes, "SPAN")[0].textContent = defValue;
+				}
 			}
 		}
-	}
+	}, 10);
 }
 
 
