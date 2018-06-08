258ab421611749061781d6868d6474ee685aeade
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-08 13:17:11 +0200

    Update Abyssus_Tools_enDev.js

--- Abyssus_Tools_enDev.js
+++ Abyssus_Tools_enDev.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 07/06/2018 20h26 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 08/06/2018 13h16 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -368,20 +368,20 @@ function onClickButtonReplaceArmy(){
 function page_playerProfile(){
 	getElementByInnerText('a', "Attaquer le terrain").onclick = function(){onclick_tmAttack()};
 				
-	var playerTM = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].text.split('(')[0];
+	var playerTM = document.getElementsByTagName('tbody')[1].childNodes[5].childNodes[3].textContent.split('(')[0];
 	playerTM = playerTM.substr(0, playerTM.length-1);
 	var maxTM = (Number(playerTM.replace(/\s/g, ''))+1).nombreFormate(0);
 
 	$.post('ajax/ennemies.php', {mintdc:playerTM, maxtdc:maxTM, page:1, tri:'distance', sens:'asc', guerre:0, paix:0, ally:0}, function(data){
-		var listOfResults = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].text);
+		var listOfResults = setPlayerTravelTime(data, "playerName:"+document.getElementsByTagName('h1')[0].textContent);
 		if(listOfResults === null){
-			document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].text += " Distance et temps de Trajet inconnus..";
+			document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].textContent += " Distance et temps de Trajet inconnus..";
 		}
 		else{
 			var distance = listOfResults[3];
 			var time = listOfResults[4];
 
-			document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].text += '\r'+ "Distance: " + distance + " _Temps de trajet: " + time;
+			document.getElementsByTagName('tbody')[1].childNodes[3].childNodes[3].textContent += '\r'+ "Distance: " + distance + " _Temps de trajet: " + time;
 		}
 	});
 }
@@ -1029,7 +1029,7 @@ function page_exploration(){
 		insertContainer.insertBefore(textUnif, insertPlace);
 
 		var buttonPrepareExploration = document.createElement('button');
-		buttonPrepareExploration.text = "Préparer les chasses";
+		buttonPrepareExploration.textContent = "Préparer les chasses";
 		buttonPrepareExploration.onclick = function(){onclickButtonExplo()};
 		insertContainer.insertBefore(buttonPrepareExploration, insertPlace);
 
