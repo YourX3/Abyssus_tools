5c87b311711563651e26156a4984cf621d28c605
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-11 20:54:40 +0200

    Update Abyssus_Tools_enDev.js

--- Abyssus_Tools_enDev.js
+++ Abyssus_Tools_enDev.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 11/06/2018 18h49 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 11/06/2018 20h49 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -153,7 +153,7 @@ function putAllUnitsToNull() {
 }
 
 function onClick_buttonFloods(){
-    	var targetTM = Number(removeSpaces(document.getElementsByName('targetTM')[0].value));
+	var targetTM = Number(removeSpaces(document.getElementsByName('targetTM')[0].value));
 	var nbRem = Number(removeSpaces(document.getElementsByName('SJ')[0].value));
 	var playerTM = Number(removeSpaces(document.getElementsByTagName('span')[7].childNodes[1].data));
 	var ghost = document.getElementById('checkBoxGhost').checked;
@@ -211,23 +211,24 @@ function onClick_buttonFloods(){
             }
         }
 
-        createCookie("numberOfAttaks", String(listOfAttaks.length), 15);
-        createCookie("attakNum", "1", 15);
+	createCookie("numberOfAttaks", String(listOfAttaks.length), 15);
+	createCookie("attakNum", "1", 15);
 	var textToAlert = "Nb d'attaques : " + String(listOfAttaks.length) + "\n";
-        for(var i=0; i < listOfAttaks.length; ++i){
-            textToAlert += "Attaque " + String((listOfAttaks[i])[0]) + ": " + String((listOfAttaks[i])[1]) + "\n";
-            createCookie(String((listOfAttaks[i])[0]), String((listOfAttaks[i])[1]), 15);
-        }
+	for(var i=0; i < listOfAttaks.length; ++i){
+	    textToAlert += "Attaque " + String((listOfAttaks[i])[0]) + ": " + String((listOfAttaks[i])[1]) + "\n";
+	    createCookie(String((listOfAttaks[i])[0]), String((listOfAttaks[i])[1]), 15);
+	}
 	textToAlert += "Total: " + String(totalFloods) + "\n" + "\n";
 	textToAlert += "Votre TM: " + String(playerTM) + "\n";
 	textToAlert += "TM de la cible: " + String(targetTM);
-	
+
 	alert(textToAlert);
-		
+
 	putAllUnitsToNull();
 	document.getElementsByName('SJ')[0].value = readCookie("Attaque_"+readCookie("attakNum"));
 	document.getElementsByName('SJ')[0].data = readCookie("Attaque_"+readCookie("attakNum"));
 	createCookie("attakNum", String(Number(readCookie("attakNum"))+1), 15);
+	$("input[value='Attaquer']").click();
     }
 }
 
@@ -1072,7 +1073,7 @@ function page_exploration(){
 		insertContainer.insertBefore(createLine(), insertPlace);
 	}
 	else {
-		if(Number(readCookie("exploCurrent")) < Number(readCookie("exploTotal"))){
+		if(Number(readCookie("exploCurrent")) <= Number(readCookie("exploTotal"))){
 			var textLanc =  document.createTextNode("Lancement d'explorations en cours... " + readCookie("exploCurrent") + "/" + readCookie("exploTotal"));
 			insertContainer.insertBefore(textLanc, insertPlace);
 
