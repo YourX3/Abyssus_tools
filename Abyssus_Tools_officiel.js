799c2f0320fc473d0bcf54fbca0abd284cff123f
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-12 20:29:53 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 12/06/2018 18h47 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.5 __ Last Updtate 12/06/2018 20h07 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -840,7 +840,7 @@ function page_membres(){
 		columnTime.align ="center";
 		document.getElementsByTagName('tbody')[1].childNodes[1].appendChild(columnTime);
 
-		var insertContainer = document.getElementsByTagName("bloc")[0];
+		var insertContainer = document.getElementsById("bloc");
 		var insertPlace = document.getElementById("tableaumembre");
 
 		var divChangePosition = document.createElement('div');
@@ -867,6 +867,71 @@ function page_membres(){
 		sessionStorage.setItem("displayingDistances_current", nbCurrentDisplaying);
 		document.location.href = href;
 	}
+	else {
+		var columnDistance = document.createElement('td');
+		columnDistance.innerHTML = '<td align="center"><td align="center"><a onclick="onClick_membersDistanceAsc()"><img src="images/asc.png" style="vertical-align: middle;"></a><strong> Distance </strong><a onclick="onClick_membersDistanceDesc()"><img src="images/desc.png" style="vertical-align: middle;"></a></td></td>';
+		columnDistance.align ="center";
+		document.getElementsByTagName('tbody')[1].childNodes[1].appendChild(columnDistance);
+
+
+		var columnTime = document.createElement('td');
+		columnTime.innerHTML = '<td align="center"><td align="center"><a onclick="onClick_membersTimeAsc()"><img src="images/asc.png" style="vertical-align: middle;"></a><strong> Temps de trajet </strong><a onclick="onClick_membersTimeDesc()"><img src="images/desc.png" style="vertical-align: middle;"></a></td></td>';
+		columnTime.align ="center";
+		document.getElementsByTagName('tbody')[1].childNodes[1].appendChild(columnTime);
+
+		var insertContainer = document.getElementsById("bloc");
+		var insertPlace = document.getElementById("tableaumembre");
+
+		var divChangePosition = document.createElement('div');
+		divChangePosition.align = "right";
+
+		var buttonChangePosition = document.createElement('button');
+		buttonChangePosition.textContent = "Se placer en temps que ";
+		buttonChangePosition.onclick = function(){onClick_buttonChangePosition()};
+		divChangePosition.appendChild(buttonChangePosition);
+		divChangePosition.innerHTML += '<input type="text" id="inputPlayerName" class="text" value="" style="font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; color: rgb(0, 0, 102); text-align: center; outline: none; padding: 5px; width: 120px; cursor: text;">';
+
+		insertContainer.insertBefore(divChangePosition, insertPlace);
+		insertContainer.insertBefore(createLine(), insertPlace);
+		insertContainer.insertBefore(createLine(), insertPlace);
+		
+		var targetPos = sessionStorage.getItem("displayingDistances_-1");
+		var allPos = [];
+		
+		for(var i=0; i < String(sessionStorage.getItem("displayingDistances_total")); ++i){
+			if(sessionStorage.getItem("displayingDistances_" + String(i)) !== null)
+				allPos.push(sessionStorage.getItem("displayingDistances_" + String(i)));
+		}
+		
+		setDistanceAndTime_byPos(targetPos, allPos);
+	}
+}
+
+function setDistanceAndTime_byPos(targetPos, playersPos){
+	var listOfPlayersTr = getPlayersTrMembers();
+	var posTargetX = targetPos.split("_")[0];
+	var posTargetY = targetPos.split("_")[1];
+	
+	
+	for(var i=0; i < listOfPlayersTr.length; ++i){
+		var targetPlayer = listOfPlayersTr[i];
+		var posPlayerX = playersPos[i].split("_")[0];
+		var posPlayerY = playersPos[i].split("_")[1];
+		
+		var distance = String((Number(posPlayerX) - Number(posTargetX)) * (Number(posPlayerX) - Number(posTargetX)) + (Number(posPlayerY) - Number(posTargetY)) * (Number(posPlayerY) - Number(posTargetY)));
+		var time = Math.ceil(Math.round((24 * 3600 * ((1 - Math.exp(-(Math.sqrt(Math.pow(Math.abs(posTargetX - posPlayerX), 2) + Math.pow(Math.abs(posTargetY - posPlayerY), 2))) / 350)) * 7.375 * Math.pow(0.9, 10)))));
+
+		var distanceTd = document.createElement('td');
+		distanceTd.align = "center";
+		distanceTd.textContent = distance;
+		targetPlayer.appendChild(distanceTd);
+
+		var timeTd = document.createElement('td');
+		timeTd.align = "center";
+		timeTd.textContent = time;
+		targetPlayer.appendChild(timeTd);
+	}
+	
 }
 
 function setDistanceAndTime_Members(){
