79e2e337920e3c4438bcff0572be4f58a52dbe9a
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-13 22:15:50 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 13/06/2018 21h18 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 13/06/2018 22h08 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -844,6 +844,9 @@ function page_membres(){
 		divChangePosition.innerHTML += '<button onclick="onClick_buttonChangePosition()">Se placer en temps que </button>';
 		divChangePosition.innerHTML += '<input type="text" id="inputPlayerName" class="text" value="" style="font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; color: rgb(0, 0, 102); text-align: center; outline: none; padding: 5px; width: 120px; cursor: text;">';
 
+		divChangePosition.appendChild(document.createTextNode(" __ VC: "));
+		divChangePosition.innerHTML += '<input type="text" id="inputVC" class="text" value="10" style="font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; color: rgb(0, 0, 102); text-align: center; outline: none; padding: 5px; width: 70px; cursor: text;">';
+		
 		insertContainer.insertBefore(divChangePosition, insertPlace);
 		insertContainer.insertBefore(createLine(), insertPlace);
 
@@ -905,6 +908,7 @@ function setDistanceAndTime_byPos(targetPos, playersPos){
 	var posTargetX = targetPos.split("_")[0];
 	var posTargetY = targetPos.split("_")[1];
 	
+	var vc = Number(sessionStorage.getItem("displayingDistances_vc"));
 	
 	for(var i=0; i < listOfPlayersTr.length; ++i){
 		var targetPlayer = listOfPlayersTr[i];
@@ -912,7 +916,7 @@ function setDistanceAndTime_byPos(targetPos, playersPos){
 		var posPlayerY = playersPos[i].split("_")[1];
 		
 		var distance = String(Math.ceil(Math.sqrt((Number(posPlayerX) - Number(posTargetX)) * (Number(posPlayerX) - Number(posTargetX)) + (Number(posPlayerY) - Number(posTargetY)) * (Number(posPlayerY) - Number(posTargetY)))));
-		var time = Math.ceil(Math.round((24 * 3600 * ((1 - Math.exp(-(Math.sqrt(Math.pow(Math.abs(posTargetX - posPlayerX), 2) + Math.pow(Math.abs(posTargetY - posPlayerY), 2))) / 350)) * 7.375 * Math.pow(0.9, 10)))));
+		var time = Math.ceil(Math.round((24 * 3600 * ((1 - Math.exp(-(Math.sqrt(Math.pow(Math.abs(posTargetX - posPlayerX), 2) + Math.pow(Math.abs(posTargetY - posPlayerY), 2))) / 350)) * 7.375 * Math.pow(0.9, vc)))));
 
 		time = displayingTime(time);
 		
@@ -932,25 +936,29 @@ function setDistanceAndTime_byPos(targetPos, playersPos){
 function displayingTime(time){
 	var result = "";
 	
-	if(Math.floor(time / 86400) > 0){
-		result = String(Math.floor(time / 86400)) + "j ";
-		time -= Math.floor(time / 86400) * 86400;
+	if(time === 0){
+		result = "0m 0s";
 	}
-	
-	if(Math.floor(time / 3600) > 0){
-		result += String(Math.floor(time / 3600)) + "h ";
-		time -= Math.floor(time / 3600) * 3600;
+	else {
+		if(Math.floor(time / 86400) > 0){
+			result = String(Math.floor(time / 86400)) + "j ";
+			time -= Math.floor(time / 86400) * 86400;
+		}
+
+		if(Math.floor(time / 3600) > 0){
+			result += String(Math.floor(time / 3600)) + "h ";
+			time -= Math.floor(time / 3600) * 3600;
+		}
+
+		if(Math.floor(time / 60) > 0){
+			result += String(Math.floor(time / 60)) + "m ";
+			time -= Math.floor(time / 60) * 60;
+		}
+
+		if(time > 0){
+			result += String(time) + "s";
+		}
 	}
-	
-	if(Math.floor(time / 60) > 0){
-		result += String(Math.floor(time / 60)) + "m ";
-		time -= Math.floor(time / 60) * 60;
-	}
-	
-	if(time > 0){
-		result += String(time) + "s";
-	}
-	
 	return result;
 }
 
@@ -1006,6 +1014,13 @@ function onClick_buttonChangePosition(){
 	if(inputValue != ""){
 		sessionStorage.setItem("displayingDistances_total", String(getPlayersTrMembers().length));
 		sessionStorage.setItem("displayingDistances_current", "-1");
+		
+		var inputVcValue = Number(document.getElementById("inputVC").value);
+		if(isNaN(inputVcValue))
+			inputVcValue = 10;
+		
+		sessionStorage.setItem("displayingDistances_vc", String(inputVcValue));
+		
 		document.location.href ="jeu.php?page=joueur&pseudo=" + inputValue;
 	}
 }
