49c3645ac3684e0ce91266783c381f99e7c52aea
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-06-13 21:14:06 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 13/06/2018 20h18 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 13/06/2018 21h18 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -911,7 +911,7 @@ function setDistanceAndTime_byPos(targetPos, playersPos){
 		var posPlayerX = playersPos[i].split("_")[0];
 		var posPlayerY = playersPos[i].split("_")[1];
 		
-		var distance = String(Math.floor((Number(posPlayerX) - Number(posTargetX)) * (Number(posPlayerX) - Number(posTargetX)) + (Number(posPlayerY) - Number(posTargetY)) * (Number(posPlayerY) - Number(posTargetY))));
+		var distance = String(Math.ceil(Math.sqrt((Number(posPlayerX) - Number(posTargetX)) * (Number(posPlayerX) - Number(posTargetX)) + (Number(posPlayerY) - Number(posTargetY)) * (Number(posPlayerY) - Number(posTargetY)))));
 		var time = Math.ceil(Math.round((24 * 3600 * ((1 - Math.exp(-(Math.sqrt(Math.pow(Math.abs(posTargetX - posPlayerX), 2) + Math.pow(Math.abs(posTargetY - posPlayerY), 2))) / 350)) * 7.375 * Math.pow(0.9, 10)))));
 
 		time = displayingTime(time);
@@ -938,17 +938,17 @@ function displayingTime(time){
 	}
 	
 	if(Math.floor(time / 3600) > 0){
-		result = String(Math.floor(time / 3600)) + "h ";
+		result += String(Math.floor(time / 3600)) + "h ";
 		time -= Math.floor(time / 3600) * 3600;
 	}
 	
 	if(Math.floor(time / 60) > 0){
-		result = String(Math.floor(time / 60)) + "m ";
+		result += String(Math.floor(time / 60)) + "m ";
 		time -= Math.floor(time / 60) * 60;
 	}
 	
 	if(time > 0){
-		result = String(time) + "s";
+		result += String(time) + "s";
 	}
 	
 	return result;
