ec16732e52c024591a07a2422bc1e0ea04752df2
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-07-02 09:26:47 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 01/07/2018 21h59 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.6 __ Last Updtate 02/07/2018 09h59 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -192,6 +192,7 @@ function onClick_buttonFloods(){
 	var targetTM = Number(removeSpaces(document.getElementsByName('targetTM')[0].value));
 	var playerTM = Number(removeSpaces(document.getElementsByTagName('span')[7].childNodes[1].data));
 	var nbRem = Number(removeSpaces(document.getElementsByName('SJ')[0].value));
+	var multi = document.getElementById('checkBoxMulti').checked;
 	
 	if(nbRem > 0 && !isNaN(targetTM)){
 		var currentAtks = document.getElementsByTagName("i");    
@@ -270,7 +271,6 @@ function removeAll_nonNbOrSpaces(str){
 function optiFlood(playerTM, targetTM){
 	var nbRem = Number(removeSpaces(document.getElementsByName('SJ')[0].value));
 	var ghost = document.getElementById('checkBoxGhost').checked;
-	var multi = document.getElementById('checkBoxMulti').checked;
 	var poseidonQuestLv = Number(removeSpaces(document.getElementById('inputQuestLv').value));
 	if(isNaN(poseidonQuestLv))
 		poseidonQuestLv = 0;
@@ -580,12 +580,6 @@ function onClick_buttonChangePosition_profile(){
 		
 		sessionStorage.setItem("displayingDistances_vc", String(inputVcValue));
 		sessionStorage.setItem("displayingDistances_finalHref", document.location.href);
-		
-		var listOfPlayersTr = getPlayersTr();
-		
-		for(var i=0; i < listOfPlayersTr.length; ++i){
-			sessionStorage.setItem("displayingDistancesPlayers_" + String(i), getElementsByTagNameInList(getElementsByTagNameInList(listOfPlayersTr[i].childNodes, "TD")[0].childNodes, "A")[0].textContent);
-		}
 		
 		document.location.href ="jeu.php?page=joueur&pseudo=" + inputValue;
 	}
