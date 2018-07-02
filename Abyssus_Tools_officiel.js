503d56ea0b4c4c70564d4cf4f1ce9099e8b68212
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-07-02 17:19:03 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.7 __ Last Updtate 02/07/2018 15h45 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.7 __ Last Updtate 02/07/2018 17h45 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -209,7 +209,7 @@ function onClick_buttonFloods(){
 					if(time !== "inconnu"){
 						var indexsOfAtks = [];
 						$(".restedans").each(function(index) {
-							var currentAtkTime = $(this).textContent;
+							var currentAtkTime = this.textContent;
 							currentAtkTime = currentAtkTime.split(" ");
 							var timeValue = "";
 							for(var i=currentAtkTime.length-1; i > 0; i-=2){
