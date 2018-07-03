b811c8c31625c849398b878167306fba1cb9658b
Youri Menubarbe <39052073+YourX3@users.noreply.github.com>
2018-07-03 15:34:07 +0200

    Update Abyssus_Tools_officiel.js

--- Abyssus_Tools_officiel.js
+++ Abyssus_Tools_officiel.js
@@ -15,7 +15,7 @@ function init(){
 	Number.prototype.nombreFormate = function(decimales,signe,separateurMilliers){var _sNombre=String(this),i,_sRetour="",_sDecimales="";if(decimales==undefined)decimales=2;if(signe==undefined)signe='';if(separateurMilliers==undefined)separateurMilliers=' ';function separeMilliers(sNombre){var sRetour="";while(sNombre.length%3!=0){sNombre="0"+sNombre}for(i=0;i<sNombre.length;i+=3){if(i==sNombre.length-1)separateurMilliers='';sRetour+=sNombre.substr(i,3)+separateurMilliers}while(sRetour.substr(0,1)=="0"){sRetour=sRetour.substr(1)}return sRetour.substr(0,sRetour.lastIndexOf(separateurMilliers))}if(_sNombre==0){_sRetour=0}else{if(_sNombre.indexOf('.')==-1){for(i=0;i<decimales;i++){_sDecimales+="0"}_sRetour=separeMilliers(_sNombre)+signe+_sDecimales}else{var sDecimalesTmp=(_sNombre.substr(_sNombre.indexOf('.')+1));if(sDecimalesTmp.length>decimales){var nDecimalesManquantes=sDecimalesTmp.length-decimales;var nDiv=1;for(i=0;i<nDecimalesManquantes;i++){nDiv*=10}_sDecimales=Math.round(Number(sDecimalesTmp)/nDiv)}_sRetour=separeMilliers(_sNombre.substr(0,_sNombre.indexOf('.')))+String(signe)+_sDecimales}}return _sRetour}
 	
 	var textVersion = document.createElement('none');
-	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.7 __ Last Updtate 03/07/2018 13h05 </font>';
+	textVersion.innerHTML = '<font size="1" color="white">Abyssus Tools V 0.7 __ Last Updtate 03/07/2018 15h05 </font>';
 	document.getElementById('footer').insertBefore(textVersion, document.getElementById('footer').childNodes[0]);
 	
 	// fin de l'URL : sur https://s1.abyssus.games/jeu.php?page=armee : ?page=armee
@@ -212,8 +212,8 @@ function onClick_buttonFloods(){
 							var currentAtkTime = this.textContent;
 							currentAtkTime = currentAtkTime.split(" ");
 							var timeValue = "";
-							for(var i=currentAtkTime.length-1; i > 0; i-=2){
-								currentAtkTime[i-1] = currentAtkTime[i-1].replace(/\D/g,'');
+							for(var i=currentAtkTime.length-2; i > 0; i-=2){
+								currentAtkTime[i] = currentAtkTime[i].replace(/\D/g,'');
 								
 								if(currentAtkTime.length === 4){
 									switch(i){
@@ -262,7 +262,7 @@ function onClick_buttonFloods(){
 								}
 								
 								
-								timeValue = currentAtkTime[i-1] + " " + timeValue;
+								timeValue = currentAtkTime[i] + " " + timeValue;
 							}
 							if(strToNumber_time(timeValue) <= time){
 								indexsOfAtks.push(index);
